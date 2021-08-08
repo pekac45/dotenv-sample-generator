71f55c72fe8d76ab9b22553986ce325ce2aea6d5
Michal Pekar <pekac45@gmail.com>
2021-08-08 14:51:57 +0100

    read and write

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const readline = require("readline");
+const splitLine = require("./lib/splitLine");
+
+const fileStream = fs.createWriteStream(".env-sample");
+
+const rl = readline.createInterface({
+	input: fs.createReadStream(".env"),
+	crlfDelay: Infinity,
+});
+
+rl.on("line", (line) => {
+	console.log(`Line from file: ${line}`);
+
+	if (line.length === 0) {
+		fileStream.write("\n");
+	} else {
+		fileStream.write(splitLine(line) + "\n");
+	}
+});
