8b747368f8be5ff3de30a1cf46c262e6198205e6
Michal Pekar <pekac45@gmail.com>
2021-08-08 15:33:34 +0100

    cleanup

--- cli.js
+++ cli.js
@@ -1,7 +1,5 @@
 #! /usr/bin/env node
 
-console.log("console.log output");
-
 const fs = require("fs");
 const readline = require("readline");
 const splitLine = require("./lib/splitLine");
@@ -14,8 +12,6 @@ const rl = readline.createInterface({
 });
 
 rl.on("line", (line) => {
-	console.log(`Line from file: ${line}`);
-
 	if (line.length === 0) {
 		fileStream.write("\n");
 	} else {
